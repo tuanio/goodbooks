8f3ee556a55589d6c0ccea84c993094d3c8fb962
TCSMFrogdance <58550812+TCSMFrogdance@users.noreply.github.com>
2021-12-01 15:16:35 +0700

    delete package repository

--- app/src/main/java/iuh/fivet/app_dev/goodbooks/repository/repository.kt
+++
@@ -1,8 +0,0 @@
-package iuh.fivet.app_dev.goodbooks.repository
-
-import iuh.fivet.app_dev.goodbooks.api.ApiService
-
-class repository(private val Api: ApiService) {
-
-    suspend fun getTop100Books() = Api.getTop100Books()
-}
